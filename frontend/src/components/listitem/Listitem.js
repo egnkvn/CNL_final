e14114fa36a7980df14a544b17206b67b8dd6eba
egnkvn <b09902021@ntu.edu.tw>
2023-05-29 22:03:03 +0800

    merge

--- frontend/src/components/listitem/Listitem.js
+++ frontend/src/components/listitem/Listitem.js
@@ -18,22 +18,10 @@ export default function Listitem({ index }) {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <Link to="/VideoPlayground">
-        <img
-          src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
-          alt=""
-        />
-      </Link>
-      {/* {isHovered && (
-          <div>
-            <Link to="/VideoPlayground">
-              <img 
-                src="https://cooley.rcsdk8.org/sites/main/files/imagecache/hero/main-images/camera_lense_0.jpeg"
-                alt=""
-              />
-            </Link>
-          </div>
-      )} */}
+      <img
+        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+        alt=""
+      />
     </div>
   );
 }
