99123b83eb1208941a1c0b8f966dc93b1626b6d5
egnkvn <b09902021@ntu.edu.tw>
2023-05-31 01:20:04 +0800

    fix: 🐛 fix some bug & UI

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 4000;
 
@@ -12,11 +16,15 @@ app.use(express.json());
 import mainRoutes from "./Controller/index.js";
 import videoRoutes from "./Controller/video.js";
 
-app.use("/", mainRoutes);
-app.use("/video", videoRoutes);
+app.use("/api/", mainRoutes);
+app.use("/api/video", videoRoutes);
 
 app.use((err, req, res, next) => {
   res.status(err.status).send(`${err.status} - ${err.message}`);
+});
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => {
